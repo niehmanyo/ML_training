8f25c2f93e22fda286f084683d323dd65afb67a9
niehmanyo <1695028510@qq.com>
2023-02-23 18:31:18 +0800

    data distribution

--- py_tutorials/Matplotlib_tutorials/Matplotlib_tuto.py
+++ py_tutorials/Matplotlib_tutorials/Matplotlib_tuto.py
@@ -3,9 +3,11 @@ import numpy as np
 
 xpoint = np.array([0,1,2,3,4])
 ypoint = np.array([0,1,4,9,16])
-plt.plot(xpoint,ypoint,scalex=20,scaley=20)
-plt.title = "X-Y coordinate system"
-
+plt.plot(xpoint,ypoint,ls = 'dashed') # ls - linestyle
+plt.title("X-Y coordinate system",loc="right") # 图的title,loc-location 的参数
+plt.xlabel("x-cm") # x的label
+plt.ylabel("y-cm") # y的label
+plt.grid() # 图的网格
 plt.show()
 
 def function(x):
